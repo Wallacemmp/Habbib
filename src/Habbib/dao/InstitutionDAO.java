f8ca50efaf160cc0be5d28578d417addd8e39d41
Wallace <wallace.mmp@hotmail.com>
2020-04-22 02:12:35 -0300

    Segregacão da conexão com o banco de dados, utilizando o package DAO e implementado melhorias nos tratamentos de erros no login.

--- src/Habbib/dao/InstitutionDAO.java
+++ src/Habbib/dao/InstitutionDAO.java
@@ -1,7 +1,6 @@
 package Habbib.dao;
 
 import Habbib.connection.ConnectionFactory;
-import Habbib.view.ViewLogin;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
