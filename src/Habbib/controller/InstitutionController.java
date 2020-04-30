0105a32276ee42780895777454f72268dfa51bfa
Wallace <wallace.mmp@hotmail.com>
2020-04-30 14:47:50 -0300

    Merge entre feature/login e DAO_Classes

--- src/Habbib/controller/InstitutionController.java
+++ src/Habbib/controller/InstitutionController.java
@@ -5,50 +5,37 @@ import Habbib.model.Address;
 import Habbib.model.Institution;
 import org.jetbrains.annotations.NotNull;
 
-public class InstitutionController
-{
-        public void Register(@NotNull Institution institution) throws Exception
-        {
-                //TODO corrigir lógica de validação da instituição já cadastrada
-                try(InstitutionDAO institutionDAO = new InstitutionDAO())
-                {
-                        Institution institutionNome = institutionDAO.getInstitutionByName(institution.getNome());
-                        Institution institutionCnpj = institutionDAO.getInstitutionByCNPJ(institution.getCnpj());
+public class InstitutionController {
+    public void Register(@NotNull Institution institution) throws Exception {
 
-                        if(institutionNome == null)
-                        {
-                                if(institutionCnpj == null)
-                                {
-                                        institutionDAO.insertInstitution(institution);
-                                }
-                        }
-                        else
-                        {
-                                if(institutionNome.getNome().equals(institution.getNome()))
-                                {
-                                        throw new Exception("Instituição com o nome " + institution.getNome() + " já cadastrada.");
-                                }
-                                else
-                                {
-                                        if(institutionCnpj.getCnpj().equals(institution.getCnpj()))
-                                        {
-                                                throw new Exception("Instituição com o CNPJ" + institution.getCnpj() + " já cadastrada.");
-                                        }
-                                        else
-                                        {
-                                                institutionDAO.insertInstitution(institution);
-                                        }
-                                }
-                        }
-                }
-        }
+        //TODO corrigir lógica de validação da instituição já cadastrada
+        try (InstitutionDAO institutionDAO = new InstitutionDAO()) {
 
-        public int addAddress(@NotNull Address address) throws Exception
-        {
-                try(InstitutionDAO institutionDAO = new InstitutionDAO())
-                {
-                        address.setId(institutionDAO.insertAddress(address));
-                        return address.getId();
+            Institution institutionNome = institutionDAO.getInstitutionByName(institution.getNome());
+            Institution institutionCnpj = institutionDAO.getInstitutionByCNPJ(institution.getCnpj());
+
+            if (institutionNome == null) {
+                if (institutionCnpj == null) {
+                    institutionDAO.insertInstitution(institution);
                 }
+            } else {
+                if (institutionNome.getNome().equals(institution.getNome())) {
+                    throw new Exception("Instituição com o nome " + institution.getNome() + " já cadastrada.");
+                } else {
+                    if (institutionCnpj.getCnpj().equals(institution.getCnpj())) {
+                        throw new Exception("Instituição com o CNPJ" + institution.getCnpj() + " já cadastrada.");
+                    } else {
+                        institutionDAO.insertInstitution(institution);
+                    }
+                }
+            }
         }
+    }
+
+    public int addAddress(@NotNull Address address) throws Exception {
+        try (InstitutionDAO institutionDAO = new InstitutionDAO()) {
+            address.setId(institutionDAO.insertAddress(address));
+            return address.getId();
+        }
+    }
 }
