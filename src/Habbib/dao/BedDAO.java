792dcba7c863ea020616322acae7a3b4aeb9dbb2
Wallace <wallace.mmp@hotmail.com>
2020-05-01 23:06:30 -0300

    Commit de att

--- src/Habbib/dao/BedDAO.java
+++ src/Habbib/dao/BedDAO.java
@@ -6,7 +6,6 @@ import Habbib.model.Institution;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
