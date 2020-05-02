5a7bd26d3fdb0bd42c38a26f550eb78ebe4528e0
Donald Barbosa <ecomponent.dbarbosa@voegol.com.br>
2020-05-02 20:44:20 -0300

    Finalização do método de cadastro da instituição e integração do método com a tela (Não foi terminada)
    --Ajuste na seleção do UF e Type. Antes estava hard coded

--- src/Habbib/controller/RequisitionController.java
+++ src/Habbib/controller/RequisitionController.java
@@ -1,4 +1,13 @@
 package Habbib.controller;
 
+import Habbib.model.Requisition;
+
+import java.util.ArrayList;
+
 public class RequisitionController {
+
+    public ArrayList<Requisition> createRequisition() throws Exception{
+
+    }
+
 }
