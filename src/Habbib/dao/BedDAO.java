cadf5139c6a9dd19a3ff00321b58dcd09bfa83b4
Wallace <wallace.mmp@hotmail.com>
2020-05-02 20:43:35 -0300

    Adicionado método getAvailableBeds.

--- src/Habbib/dao/BedDAO.java
+++ src/Habbib/dao/BedDAO.java
@@ -1,6 +1,7 @@
 package Habbib.dao;
 
 import Habbib.connection.BaseDAO;
+import Habbib.model.Address;
 import Habbib.model.Bed;
 import Habbib.model.Institution;
 
@@ -35,6 +36,59 @@ public class BedDAO extends BaseDAO {
                 bed.setId(rs.getInt("Id"));
                 bed.setType(rs.getString("Type"));
                 bed.setStatus(rs.getString("Status"));
+                bed.setInstitution(institution);
+
+                beds.add(bed);
+
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            throw e;
+        }
+        // retorno do ArrayList carregado.
+        return beds;
+    }
+
+    public ArrayList<Bed> getAvailableBeds() throws Exception{
+
+        PreparedStatement stmt;
+        ResultSet rs;
+        ArrayList<Bed> beds;
+        Bed bed;
+        Institution institution;
+        Address address;
+
+        try{
+
+            beds = new ArrayList<>();
+            String select = "SELECT b.*,i.* FROM Bed b JOIN Institution i ON b.Id_Institution = i.Id";
+            stmt = super.connection.prepareStatement(select);
+            rs = stmt.executeQuery();
+
+            //Carrega objetos bed em seguida insere o OBJETO BED no ArrayList
+            while(rs.next()){
+
+                bed = new Bed();
+                institution = new Institution();
+                address = new Address();
+                bed.setId(rs.getInt("b.Id"));
+                bed.setType(rs.getString("b.Type"));
+                bed.setStatus(rs.getString("Status"));
+                institution.setId(rs.getInt("i.Id"));
+                institution.setName(rs.getString("Name"));
+                institution.setCnpj(rs.getString("CNPJ"));
+                institution.setPassword(rs.getString("Password"));
+                institution.setType(rs.getString("i.Type"));
+                institution.setContactNumber(rs.getString("ContactNumber"));
+                address.setId(rs.getInt("a.Id"));
+                address.setZipCode(rs.getString("ZipCode"));
+                address.setAddress(rs.getString("Address"));
+                address.setNumber(rs.getInt("AddressNumber"));
+                address.setComplement(rs.getString("Complement"));
+                address.setNeighborhood(rs.getString("Neighborhood"));
+                address.setCity(rs.getString("City"));
+                address.setUf(rs.getString("UF"));
+                institution.setAddress(address);
                 bed.setInstitution(institution);
 
                 beds.add(bed);
