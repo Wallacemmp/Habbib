c40ebed2be99526626ef87cdb59a038ca577d035
Wallace <wallace.mmp@hotmail.com>
2020-05-02 20:40:58 -0300

    Commit de att

--- src/Habbib/controller/BedController.java
+++ src/Habbib/controller/BedController.java
@@ -1,4 +1,23 @@
 package Habbib.controller;
 
+import Habbib.dao.BedDAO;
+import Habbib.model.Bed;
+
 public class BedController {
+
+    public void registerBeds(Bed bed, int count) throws Exception{
+
+        try(BedDAO bedDAO = new BedDAO()) {
+
+            for(int i = 0; i < count; i++){
+                bedDAO.addBed(bed);
+            }
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            throw e;
+        }
+    }
+
+
 }
