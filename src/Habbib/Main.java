cd8b6229f6bc78a3c065536d3165ae4c2b3fa796
arthurmpc <41999569+ArthurMPC@users.noreply.github.com>
2020-05-04 21:57:52 -0300

    Update Main.java

--- src/Habbib/Main.java
+++ src/Habbib/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         try
         {
-            View view = new View();
+            View view = new View()
         }
         catch (Exception ex)
         {
